0b08229b1bcf2a32ffaa5d29fdc52ee4067a4daa
Ronaldo <ronaldovctor@outlook.com>
2022-10-13 15:52:59 -0300

    form created

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,20 @@
-import React, { ReactNode } from 'react'
+import clsx from 'clsx'
+import React, { ButtonHTMLAttributes, ReactNode } from 'react'
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	children: ReactNode
+	className?: string
 }
 
-export function Button({ children }: ButtonProps) {
+export function Button({ children, className, ...props }: ButtonProps) {
 	return (
-		<button className='py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm hover:bg-cyan-300 cursor-pointer transition-colors focus:ring-2 ring-white'>
+		<button
+			{...props}
+			className={clsx(
+				'py-3 px-16 bg-cyan-500 rounded font-semibold text-black text-sm hover:bg-cyan-300 cursor-pointer transition-colors focus:ring-2 ring-white',
+				className
+			)}
+		>
 			{children}
 		</button>
 	)
